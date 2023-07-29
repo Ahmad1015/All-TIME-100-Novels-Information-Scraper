339cd21556d92b13fc26c4ebdd212f4d3061b7d0
Ahmad <mahmedraza90@gmail.com>
2023-07-30 03:18:35 +0500

    Update to main.py
    Stable version of my code where i am able to fetch all of the wikipedia links

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-
+# Getting the names of the books
 web_page = requests.get("https://entertainment.time.com/2005/10/16/all-time-100-novels/slide/all/").text
 
 soup = BeautifulSoup(web_page,"html.parser")
@@ -11,6 +11,7 @@ div_tag = soup.find(class_="entry-content group")
 
 names = [item.getText() for item in div_tag.find_all("i")]
 
+# Getting the wikipedia link of the books
 wikipedia_link = []
 
 for item in names:
@@ -28,7 +29,6 @@ for item in names:
 
     wikipedia_link.append(response[-1])
 
-    print(response[-1])
 
 
 
@@ -39,23 +39,9 @@ for item in names:
 
 
 
-'''wiki_wiki = wikipediaapi.Wikipedia('MyProjectName (merlin@example.com)', 'en')
-
-link_list = []
 
 
 
-
-for x in range(0,len(names)):
-    page_py = wiki_wiki.page(names[x])
-    print("Page - Exists: %s" % page_py.exists())
-    if not page_py.exists():
-        print(names[x])
-print(page_py.fullurl)
-
-page_py = wiki_wiki.page('Python_(programming_language)')
-print("Page - Exists: %s" % page_py.exists())
-link_list.append(page_py.fullurl)'''
 
 
 

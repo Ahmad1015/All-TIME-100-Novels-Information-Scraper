d824512c0042cec59eb76f2085bcc63c962c3ee9
Ahmad <mahmedraza90@gmail.com>
2023-07-30 04:35:09 +0500

    update

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from bs4 import BeautifulSoup
 import requests
 
-
 # Getting the names of the books
 web_page = requests.get("https://entertainment.time.com/2005/10/16/all-time-100-novels/slide/all/").text
 
-soup = BeautifulSoup(web_page,"html.parser")
+soup = BeautifulSoup(web_page, "html.parser")
 
 div_tag = soup.find(class_="entry-content group")
 
@@ -29,22 +28,24 @@ for item in names:
 
     wikipedia_link.append(response[-1])
 
+# Getting relevant information of specific books from wikipedia
+relevant_data = []
+for index in range(len(wikipedia_link)):
+    temp = wikipedia_link[index]
+    wiki_data = requests.get(str(temp)).text
+    soup = BeautifulSoup(wiki_data, "html.parser")
+    t_body_element = soup.find(name="tbody")
+    panel = t_body_element.find_all(name="td", class_="infobox-data")
 
+    dict = {
+        'author': t_body_element.find(class_="infobox-data").getText(),
+        'language': panel[2].getText(),
+        'genre': panel[3].getText(),
+        'publisher': panel[4].getText(),
+        'publication_date': panel[5].getText()[:-3],
+        'print_available': panel[6].getText(),
+        'pages': panel[7].getText(),
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    relevant_data.append(dict)
+print(relevant_data)
